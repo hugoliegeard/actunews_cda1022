d4c32500ed2e55d39c58fa857c49bc825e29ffe4
Cogitium <info@cogitium.com>
2023-02-23 17:55:43 +0100

    23/02/2023 Config BD MySQL dans .env; Création de 2 entités Category et Post; présentation des migration; Make d'un form pour Category et utilisation dans une vue Twig pour créer une catégory; Génération de slug avec Gedmo

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
+{
+    #[Route('/category/add', name: 'category_add')]
+    public function addCategory(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('admin/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
