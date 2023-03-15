cbe66a963183ea28a4fc35dbf6218639755657e4
Hugo LIEGEARD <hugo@hl-media.co>
2023-03-15 12:34:25 +0100

    Outils de qualité de code

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,6 +6,10 @@ use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\User;
+use App\Repository\CategoryRepository;
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,14 +19,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard_home', methods: 'GET')]
-    public function home(ManagerRegistry $em): Response
+    public function home(UserRepository $userRepository,
+                         CommentRepository $commentRepository,
+                         PostRepository $postRepository,
+                         CategoryRepository $categoryRepository): Response
     {
         # Récupération des données
-        $total['users'] = $em->getRepository(User::class)->findTotal();
-        $total['comments'] = $em->getRepository(Comment::class)->findTotal();
-        $total['categories'] = $em->getRepository(Category::class)->findTotal();
-        $total['posts'] = $em->getRepository(Post::class)->findTotal();
-        $posts = $em->getRepository(Post::class)->findBy([], ['id' => 'DESC'], 10);
+        $total['users'] = $userRepository->findTotal();
+        $total['comments'] = $commentRepository->findTotal();
+        $total['categories'] = $categoryRepository->findTotal();
+        $total['posts'] = $postRepository->findTotal();
+        $posts = $postRepository->findBy([], ['id' => 'DESC'], 10);
 
         # Transfert des données à la vue
         return $this->render('dashboard/dashboard.html.twig', [
