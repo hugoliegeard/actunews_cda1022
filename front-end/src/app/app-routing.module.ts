94c3293641ace9357746722075723d1c455083ae
Hugo LIEGEARD <hugo@hl-media.co>
2023-03-22 20:33:49 +0100

    Intégration Front-End

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./modules/pages/home/home.component";
+import {CategoryComponent} from "./modules/pages/category/category.component";
+import {PostComponent} from "./modules/pages/post/post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // http://localhost:4200
+    path: '',
+    component: HomeComponent
+  },
+  {
+    // http://localhost:4200/politique
+    path: ':alias',
+    component: CategoryComponent,
+  },
+  {
+    // http://localhost:4200/category/politique
+    path: 'category/:alias',
+    redirectTo: '/:alias',
+    pathMatch: 'full'
+  },
+  {
+    // http://localhost:4200/politique/alias-de-mon-article
+    path: ':category/:alias',
+    component: PostComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { CategoryComponent } from './modules/pages/category/category.component';
+import { PostComponent } from './modules/pages/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    HomeComponent,
+    CategoryComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
